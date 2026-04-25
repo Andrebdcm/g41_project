2a8833af0e81ad49c94d39c7632df34aa70f61e0
André <andre@local.com>
2026-04-25 10:45:04 +0100

    Initial commit

---
+++ classes/gclass.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, asdict
+from typing import Dict, Any, Optional
+
+@dataclass
+class Gclass:
+    id: Optional[int] = None
+
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
